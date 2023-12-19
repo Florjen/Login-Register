c9b6b3dda76eba8bbb7db9645aa1902539b16148
Florjen <116762837+Florjen@users.noreply.github.com>
2023-12-19 14:34:12 +0100

    Javascript-Login-Register

---
+++ login/index.js
@@ -0,0 +1,8 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('psw');
+    var showPasswordCheckbox = document.getElementById('showPassword');
+
+    // Ndrysho tipin e input-it nga password në text dhe anasjelltas
+    passwordInput.type = showPasswordCheckbox.checked ? 'text' : 'password';
+
+  }
